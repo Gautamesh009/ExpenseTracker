0851389b41004a33a522f85df22495c72cda163a
Gautamesh009 <gautameshz@gmail.com>
2026-03-13 19:24:46 +0530

    UI Working

--- app/src/main/java/com/example/expensetraclerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetraclerapp/MainActivity.kt
@@ -4,21 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.expensetraclerapp.UIDesign.EntryScreen
 import com.example.expensetraclerapp.UIDesign.MainScreen
 import com.example.expensetraclerapp.UIDesign.Settings
-import com.example.expensetraclerapp.ui.theme.ExpenseTraclerAppTheme
+import com.example.expensetraclerapp.ViewModel.TrackerViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,10 +27,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
+    val viewModel : TrackerViewModel = viewModel(factory = TrackerViewModel.Factory )
 
     NavHost(navController, startDestination = "EntryScreen") {
         composable("EntryScreen") { EntryScreen(navController) }
-        composable("MainScreen") { MainScreen(navController) }
-        composable("Settings") { Settings(navController) }
+        composable("MainScreen") { MainScreen(navController, viewModel) }
+        composable("Settings") { Settings(navController, viewModel) }
     }
 }
--- app/src/main/java/com/example/expensetraclerapp/UIDesign/MainScreen.kt
+++ app/src/main/java/com/example/expensetraclerapp/UIDesign/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.example.expensetraclerapp.UIDesign
 
-import android.hardware.camera2.params.BlackLevelPattern
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -14,34 +13,35 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Settings
-import androidx.compose.material.icons.sharp.Settings
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import kotlinx.coroutines.flow.combine
+import com.example.expensetraclerapp.Room.PaymentHistory
+import com.example.expensetraclerapp.ViewModel.TrackerViewModel
 
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(navController: NavController, viewModel: TrackerViewModel) {
+    val payments by viewModel.allPayments.collectAsState()
+
     Scaffold(
         content = {
             Column(modifier = Modifier
@@ -129,7 +129,7 @@ fun MainScreen(navController: NavController) {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    items(50) {
+                    items(2) {
                        Card(
                            shape = RoundedCornerShape(20.dp, 0.dp, 20.dp, 0.dp),
                            elevation = CardDefaults.elevatedCardElevation(8.dp),
@@ -139,7 +139,23 @@ fun MainScreen(navController: NavController) {
                                    Color.Black,
                                    RoundedCornerShape(20.dp, 0.dp, 20.dp, 0.dp))
                        ) {
-                            Text("")
+                           Column(
+                               modifier = Modifier.fillMaxSize()
+                                   .padding(16.dp),
+                               horizontalAlignment = Alignment.Start,
+                               verticalArrangement = Arrangement.SpaceEvenly
+                           ) {
+                               Row(
+                                   modifier = Modifier.fillMaxWidth(),
+                                   horizontalArrangement = Arrangement.SpaceBetween,
+                                   verticalAlignment = Alignment.CenterVertically
+                               ) {
+                                   Text("Payment Message :", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                                   Text("payment price", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                               }
+                               Spacer(modifier = Modifier.height(5.dp))
+                               Text("Date", fontSize = 10.sp, fontWeight = FontWeight.Light)
+                           }
                        }
                         Spacer(Modifier.height(10.dp))
                     }
